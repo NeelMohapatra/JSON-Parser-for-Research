97767b9dea628b575616cefaba9e79b69373ffdf
Swojit Mohapatra <neelmohapatra@gmail.com>
2016-11-02 18:00:57 -0700

    Changed the files for analysis of new data

--- parser.py
+++ parser.py
@@ -4,42 +4,44 @@
 
 import json, os, csv, re
 from pprint import pprint
-JSON_DIR = '/Users/swojit/Downloads/companies'
+JSON_DIR = '/Users/swojit/Downloads/final_companies'
 files = os.listdir(JSON_DIR)
-errors = []
+# errors = []
 keys = []
-markets = []
-working_files = []
-for file in files:
+# markets = []
+# working_files = []
+for file in files[:100]:
     json_file = open(JSON_DIR + '/' + file)
     data = json.load(json_file)
-    keys.extend(data.keys())
-    try:
-        markets.append(data['markets'])
-        working_files.append(data)
-    except KeyError:
-        errors.append(file)
+    pprint(data)
+    print("\n")
+    # keys.extend(data.keys())
+    # try:
+    #     markets.append(data['markets'])
+    #     working_files.append(data)
+    # except KeyError:
+    #     errors.append(file)
 keys = set(keys)
-# print(keys)
-outputfile = open('output.csv', 'w', newline='')
-outputWriter = csv.writer(outputfile)
-outputWriter.writerow([key for key in keys]) # Header
-for file in working_files:
-    row = []
-    for key in keys:
-        try:
-            row.append(file[key])
-        except KeyError:
-            row.append('NOTHING_HERE')
-    if not file['hidden']:
-       outputWriter.writerow(row)
-markets_dict = []
-count = set()
-for market in markets:
-    if len(market):
-        for individual in market:
-            markets_dict.append(individual)
-            count.add(individual['id'])
-print("Number of unique markets: " + str(len(count)))
-print("Number of outgrees: " + str(len(markets_dict)))
-outputfile.close()
+print(keys)
+#outputfile = open('output2.csv', 'w', newline='')
+#outputWriter = csv.writer(outputfile)
+#outputWriter.writerow([key for key in keys]) # Header
+# for file in working_files:
+#    row = []
+#    for key in keys:
+#        try:
+#            row.append(file[key])
+#        except KeyError:
+#             row.append('NOTHING_HERE')
+#     if not file['hidden']:
+#        outputWriter.writerow(row)
+# markets_dict = []
+# count = set()
+# for market in markets:
+#    if len(market):
+#        for individual in market:
+#            markets_dict.append(individual)
+#             count.add(individual['id'])
+# print("Number of unique markets: " + str(len(count)))
+# print("Number of outgrees: " + str(len(markets_dict)))
+# outputfile.close()
